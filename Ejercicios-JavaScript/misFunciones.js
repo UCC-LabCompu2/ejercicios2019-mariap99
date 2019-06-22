f0246122ccae6d5eef4896ce7bff0a59d9c75ba1
mariap99 <maria99merlo@gmail.com>
2019-06-22 18:32:17 -0300

    completando actividades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,11 +1,139 @@
-/**
- * Created by Agus on 6/5/2017.
- */
 
-/**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
- */
+function cambioUnidades(valor, unidad) {
+
+    if(isNaN(valor)){
+        alert("Se ingreso un valor invalido en " + unidad);
+        document.lasUnidades.yarda.value = "";
+        document.lasUnidades.metro.value = "";
+        document.lasUnidades.pulgada.value = "";
+        document.lasUnidades.pie.value = "";
+    } else if (unidad == "metro") {
+        document.lasUnidades.pulgada.value = valor * 39.3701;
+        document.lasUnidades.pie.value = valor * 3.28084;
+        document.lasUnidades.yarda.value = valor * 1.09361;
+    } else if (unidad == "pulgada") {
+        document.lasUnidades.metro.value = valor * 0.0254;
+        document.lasUnidades.pie.value = valor * 0.08333;
+        document.lasUnidades.yarda.value = valor * 0.027778;
+    } else if (unidad == "pie") {
+        document.lasUnidades.metro.value = valor * 0.3048;
+        document.lasUnidades.pulgada.value = valor * 12;
+        document.lasUnidades.yarda.value = valor * 0.333333;
+    } else if (unidad == "yarda") {
+        document.lasUnidades.metro.value = valor * 0.9144;
+        document.lasUnidades.pulgada.value = valor * 36;
+        document.lasUnidades.pie.value = valor * 3;
+    }
+}
+
+function suma()
+{
+    document.opeMat.sum_total.value= Number(document.opeMat.sum_num1.value)+ Number(document.opeMat.sum_num2.value);
+}
+
+function resta()
+{
+    document.opeMat.res_total.value= Number(document.opeMat.res_num1.value)- Number(document.opeMat.res_num2.value);
+}
+
+function mult()
+{
+    document.opeMat.mul_total.value= Number(document.opeMat.mul_num1.value)* Number(document.opeMat.mul_num2.value);
+}
+
+function div()
+{
+    document.opeMat.div_total.value= Number(document.opeMat.div_num1.value)/ Number(document.opeMat.div_num2.value);
+}
+
+function gradosRadianes(id_elem,valor){
+
+    if(id_elem==grados)
+
+    {document.getElementById("radianes").value=valor*Math.PI/180;}
+
+    if(id_elem==radianes)
+
+    {document.getElementById("grados").value=valor*180/Math.PI;}
+
+}
+
+function mostrarocultar(id_elem) {
+
+    if(id_elem =="ocultarDiv"){
+        document.getElementById("unDiv").style.display= 'none';
+
+ }   }
+
+ function dibujarCircCuad (){
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#a8538d";
+    ctx.beginPath();
+    ctx.rect(0,0,30,40);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle="#07a84f";
+    ctx.beginPath();
+    ctx.arc(canvas.width/2, canvas.height/2,30,0,2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+
+ }
+
+ function dibujarcuadriculado()
+ {
+     var canvas = document.getElementById("myCanvas");
+     var ctx = canvas.getContext("2d");
+
+     ctx.clearRect(0,0,canvas.width,canvas.height);
+
+     ctx.fillStyle="#ff5400";
+
+     ctx.beginPath();
+
+     for(var i=10; i< canvas.width; i=i+10)
+     {
+         ctx.moveTo(i,0);
+         ctx.lineTo(i,canvas.height);
+     }
+
+     for(var i=10; i<canvas.height; i=i+10)
+     {
+         ctx.moveTo(0,i);
+         ctx.lineTo(canvas.width,i);
+     }
+
+     ctx.stroke();
+     ctx.closePath();
+
+
+ }
+
+ function dibujarimg() {
+
+     var canvas = document.getElementById("myCanvas");
+     var ctx = canvas.getContext("2d");
+
+     var img= new Image();
+     img.src= "images/auto.png";
+
+     img.onload=function ()
+     {
+
+         ctx.drawImage(img,50,50);
+
+     }
+
+
+
+ }
+
+
+
+
+
